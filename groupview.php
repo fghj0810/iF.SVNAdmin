f834dcbbc23b18db697adcfca11b57c06f678d63
fghj0810 <fghj0810@gmail.com>
2021-03-10 14:37:29 +0800

    fix ldap group

--- groupview.php
+++ groupview.php
@@ -20,25 +20,22 @@
 include("include/config.inc.php");
 
 if (!$appEngine->isGroupViewActive() && !$appEngine->isAccessPathViewActive())
-  $appEngine->forwardInvalidModule(true);
+    $appEngine->forwardInvalidModule(true);
 
 $appEngine->checkUserAuthentication(true, ACL_MOD_GROUP, ACL_ACTION_VIEW);
 
 $appTR->loadModule("groupview");
 
 // Action handling.
-if (check_request_var('unassign'))
-{
-  $appEngine->handleAction('unassign_fromgroup');
+if (check_request_var('unassign')) {
+    $appEngine->handleAction('unassign_fromgroup');
 }
-if (check_request_var('unassign_permission'))
-{
-  $appEngine->handleAction('unassign_permission');
+if (check_request_var('unassign_permission')) {
+    $appEngine->handleAction('unassign_permission');
 }
-if (check_request_var('assign_usergroup') || check_request_var('assign_subgroupgroup'))
-{
-  $appTR->loadModule("groupassign");
-  $appEngine->handleAction("assign_togroup");
+if (check_request_var('assign_usergroup') || check_request_var('assign_subgroupgroup')) {
+    $appTR->loadModule("groupassign");
+    $appEngine->handleAction("assign_togroup");
 }
 
 // Get required variables.
@@ -50,45 +47,33 @@ $oGroup->id = $groupname;
 $oGroup->name = $groupname;
 
 // Users of group
-$users=null;
-$allusers=null;
-if ($appEngine->isGroupViewActive() && $appEngine->checkUserAuthentication(false, ACL_MOD_GROUP, ACL_ACTION_VIEW))
-{
-  $users = $appEngine->getGroupViewProvider()->getUsersOfGroup( $oGroup );
-  $groups = $appEngine->getGroupViewProvider()->getSubgroupsOfGroup( $oGroup );
-  usort( $users, array('\svnadmin\core\entities\User',"compare") );
-  usort( $groups, array('\svnadmin\core\entities\Group',"compare") );
-  
-  // All users except the already assigned users.
-  if ($appEngine->isUserViewActive() && $appEngine->checkUserAuthentication(false, ACL_MOD_GROUP, ACL_ACTION_ASSIGN))
-  {
-    $allusers = $appEngine->getUserViewProvider()->getUsers(false);
-    usort($allusers, array('\svnadmin\core\entities\User',"compare"));
-    $len = count($users);
-    for ($i=0; $i<$len; $i++)
-    {
-    	if_array_remove_object_element($allusers, $users[$i], "name");
-    }
-    $allusers = array_values($allusers);
+$users = null;
+$allusers = null;
+if ($appEngine->isGroupViewActive() && $appEngine->checkUserAuthentication(false, ACL_MOD_GROUP, ACL_ACTION_VIEW)) {
+    $users = $appEngine->getGroupViewProvider()->getUsersOfGroup($oGroup);
 
-    $allgroups = $appEngine->getGroupViewProvider()->getGroups();
-    usort($allgroups, array('\svnadmin\core\entities\Group',"compare"));
-    $len = count($groups);
-    for ($i=0; $i<$len; $i++)
-    {
-      if_array_remove_object_element($allgroups, $groups[$i], "name");
+    // All users except the already assigned users.
+    if ($appEngine->isUserViewActive() && $appEngine->checkUserAuthentication(false, ACL_MOD_GROUP, ACL_ACTION_ASSIGN)) {
+        $allusers = $appEngine->getUserViewProvider()->getUsers(false);
+        usort($allusers, array('\svnadmin\core\entities\User', "compare"));
+        $len = count($users);
+        for ($i = 0; $i < $len; $i++) {
+            if_array_remove_object_element($allusers, $users[$i], "name");
+        }
+        $allusers = array_values($allusers);
+
+        $allgroups = $appEngine->getGroupViewProvider()->getGroups();
+        usort($allgroups, array('\svnadmin\core\entities\Group', "compare"));
+        if_array_remove_object_element($allgroups, $oGroup, "name");
+        $allgroups = array_values($allgroups);
     }
-    if_array_remove_object_element($allgroups, $oGroup, "name");
-    $allgroups = array_values($allgroups);
-  }
 }
 
 // Access-Path permissions of the current group.
-$paths=null;
-if ($appEngine->isAccessPathViewActive() && $appEngine->checkUserAuthentication(false, ACL_MOD_ACCESSPATH, ACL_ACTION_VIEW))
-{
-  $paths = $appEngine->getAccessPathViewProvider()->getPathsOfGroup( $oGroup );
-  usort( $paths, array('\svnadmin\core\entities\AccessPath',"compare") );
+$paths = null;
+if ($appEngine->isAccessPathViewActive() && $appEngine->checkUserAuthentication(false, ACL_MOD_ACCESSPATH, ACL_ACTION_VIEW)) {
+    $paths = $appEngine->getAccessPathViewProvider()->getPathsOfGroup($oGroup);
+    usort($paths, array('\svnadmin\core\entities\AccessPath', "compare"));
 }
 
 SetValue("GroupName", $groupname);
